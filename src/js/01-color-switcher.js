b8572ec9311318d48d046361e9169a4036b796a1
mykolaromanets <125486364+mykolaromanets@users.noreply.github.com>
2023-04-27 22:12:17 +0300

    make third task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const btnStartEl = document.querySelector('[data-start]');
+console.log(btnStartEl);
+const btnStoptEl = document.querySelector('[data-stop]');
+console.log(btnStoptEl);
 
+let intervalId = null;
+
+btnStartEl.addEventListener('click', () => {
+  btnStartEl.disabled = true;
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+btnStoptEl.addEventListener('click', () => {
+  clearInterval(intervalId);
+  btnStartEl.disabled = false;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
